3cba91c883318116be645301a215814f313bc65a
DuyKhiem <khiemnvdse180168@fpt.edu.vn>
2025-10-27 02:52:27 +0700

    feat: add launch configuration for Flutter and update app title to 'CellphoneZ'

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Shop Template by The Flutter Way',
+      title: 'CellphoneZ',
       theme: AppTheme.lightTheme(context),
       themeMode: ThemeMode.light,
       onGenerateRoute: router.generateRoute,
