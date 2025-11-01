ba0751c96eef332b0077bfbbdf6cf84e5303b18f
DuyKhiem <khiemnvdse180168@fpt.edu.vn>
2025-11-02 02:52:13 +0700

    chore: resolve merge conflict in pubspec.lock

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class MyApp extends StatelessWidget {
       themeMode: ThemeMode.light,
       onGenerateRoute: router.generateRoute,
       home: const MainScreen(),
-      initialRoute: adminProductListScreenRoute,
+      initialRoute: onbordingScreenRoute,
     );
   }
 }
